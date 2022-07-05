ac281bde060f281108863556e228c80c795722c6
Mason <ab300819@163.com>
2022-07-05 18:02:07 +0800

    feat: 添加邻接矩阵

--- in-python/src/graph/graph.py
+++ in-python/src/graph/graph.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 
 class MatrixGraph:
-    def __int__(self, node_num: int, node_edge: int):
+    def __int__(self, node_num: int, node_relation: []):
         self.vex = []
         self.arc = []
         self.node_num = node_num
-        self.node_edge = node_edge
+        self.node_relation = node_relation
 
-    def create(self, node_list: []):
-        pass
-
-    def _create_from(self, ):
+    def create(self):
         pass
 
 
 if __name__ == '__main__':
-    graph = MatrixGraph()
+    node_num = 10
+    node_relation = [
+        (1, 2),
+        (1, 3),
+        (1, 5),
+    ]
+    graph = MatrixGraph(node_num, node_relation)
+    print(node_relation)
