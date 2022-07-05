5c2b88696fd5087b77f5bf66a50eb3565359ccf4
Mason <ab300819@163.com>
2022-07-05 17:18:59 +0800

    feat: 添加“图”邻接矩阵

---
+++ in-python/src/graph/graph.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+class MatrixGraph:
+    def __int__(self, node_num: int, node_edge: int):
+        self.vex = []
+        self.arc = []
+        self.node_num = node_num
+        self.node_edge = node_edge
+
+    def create(self, node_list: []):
+        pass
+
+    def _create_from(self, ):
+        pass
+
+
+if __name__ == '__main__':
+    graph = MatrixGraph()
