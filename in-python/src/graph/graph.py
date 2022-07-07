908e8831af7f556f15f76dd05d60cfffb9c6c5d1
Mason <ab300819@163.com>
2022-07-07 13:23:26 +0800

    feat: 添加邻接表版本 BFS

--- in-python/src/graph/graph.py
+++ in-python/src/graph/graph.py
@@ -1,22 +1,92 @@
 #!/usr/bin/env python3
 
+from queue import Queue
+
+
+# 邻接矩阵表示法
 class MatrixGraph:
-    def __int__(self, node_num: int, node_relation: []):
-        self.vex = []
-        self.arc = []
-        self.node_num = node_num
-        self.node_relation = node_relation
+    def __init__(self, node_num: int, node_relation: []):
+        self._arc = []
+        self._node_num = node_num
+        self._node_relation = node_relation
 
     def create(self):
-        pass
+        self._arc = [[None for i in range(self._node_num)] for i in range(self._node_num)]
+        for i in self._node_relation:
+            self._arc[i[0]][i[1]] = 1
+
+    def get_graph(self):
+        return self._arc
+
+
+# 邻接链表表示法
+class ListGraph:
+    def __init__(self, node_relation: []):
+        self._node_relation = node_relation
+        self._arc = {}
+
+    def create(self):
+        for i in self._node_relation:
+            if i[0] in self._arc:
+                self._arc[i[0]].append(i[1])
+            else:
+                self._arc[i[0]] = [i[1]]
+
+            if i[1] in self._node_relation:
+                self._arc[i[1]].append(id[0])
+            else:
+                self._arc[i[1]] = [i[0]]
+
+    def bfs(self, source: int, target: int):
+        if source == target:
+            return
+        visited = {key: False for key in self._arc.keys()}
+        queue = Queue(len(self._arc))
+        queue.put(source)
+
+        prev = {key: -1 for key in self._arc.keys()}
+        path = []
+        while not queue.empty():
+            current = queue.get()
+            for i in self._arc[current]:
+                if not visited[i]:
+                    prev[i] = current
+                    if i == target:
+                        self._print(prev, source, target, path)
+                        return path
+                    visited[i] = True
+                    queue.put(i)
+        return path
+
+    def _print(self, prev: {}, source: int, target: int, path: []):
+        if prev[target] != -1 and source != target:
+            self._print(prev, source, prev[target], path)
+        path.append(target)
+
+    def get_graph(self):
+        return self._arc
 
 
 if __name__ == '__main__':
-    node_num = 10
-    node_relation = [
+    node_num = 6
+    relation = [
+        (0, 1),
+        (0, 3),
         (1, 2),
-        (1, 3),
-        (1, 5),
+        (1, 4),
+        (2, 5),
+        (4, 5),
+        (4, 6),
+        (5, 7),
+        (6, 7)
     ]
-    graph = MatrixGraph(node_num, node_relation)
-    print(node_relation)
+    # graph = MatrixGraph(node_num, relation)
+    # graph.create()
+    # node_graph = graph.get_graph()
+    # for i in node_graph:
+    #     print(i)
+
+    graph = ListGraph(relation)
+    graph.create()
+    path = graph.bfs(0, 6)
+    print(path)
--- in-python/src/tests/graph_test.py
+++ in-python/src/tests/graph_test.py
@@ -1,22 +1,46 @@
 #!/usr/bin/env python3
 import unittest
 
+from graph.graph import ListGraph
+
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def setUp(self):
+        pass
+
+    def test_list_graph_bfs_1(self):
+        relation = [
+            (0, 1),
+        ]
+
+        graph = ListGraph(relation)
+        graph.create()
+        self.assertEqual(graph.bfs(0, 2), [])
+        self.assertEqual(graph.bfs(0, 1), [0, 1])
+
+    def test_list_graph_bfs_2(self):
+        relation = [
+            (0, 1),
+            (0, 3),
+            (1, 2),
+            (1, 4),
+            (2, 5),
+            (4, 5),
+            (4, 6),
+            (5, 7),
+            (6, 7)
+        ]
+
+        graph = ListGraph(relation)
+        graph.create()
+        self.assertEqual(graph.bfs(0, 6), [0, 1, 4, 6])
 
     def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        pass
 
     def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+        pass
 
 
 if __name__ == '__main__':
